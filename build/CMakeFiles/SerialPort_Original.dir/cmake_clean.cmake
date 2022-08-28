94315a609bd51be4f62d5a863fd64f650b9a1054
weslydreamer <1165986911@qq.com>
2022-08-28 10:23:30 +0800

    1st

---
+++ build/CMakeFiles/SerialPort_Original.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SerialPort_Original.dir/src/main.o"
+  "SerialPort_Original"
+  "SerialPort_Original.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SerialPort_Original.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
